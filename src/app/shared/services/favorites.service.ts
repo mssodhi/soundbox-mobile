ab85e0f3583160bb5ec66e1eb07b4a32f2723939
Manu SOdhi <mssodhi@ucdavis.edu>
2017-02-23 19:29:29 -0800

    minor fix

--- src/app/shared/services/favorites.service.ts
+++ src/app/shared/services/favorites.service.ts
@@ -7,7 +7,7 @@ export class FavoritesService {
   url: String = '';
 
   constructor(private http: Http, private platform: Platform) {
-    if(this.platform.is('iPhone')){
+    if(this.platform.is('iphone')){
       this.url = 'http://mssodhi.me/soundbox';
     } else {
       this.url = '/soundbox';
